abd67d54816f5465969ff7d56cb1b885a3a7d9bd
Steven Rosario <strosariogarcia@gmail.com>
2022-09-06 12:24:50 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.querySelector("body").append(data.id)
+    })
+    .catch(error => {
+        document.querySelector("body").append(error)
+    })
+}
